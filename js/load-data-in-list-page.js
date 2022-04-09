189b1df439b666609f56a31df04cc2b48830d5e5
rameem2003 <rameem2019@gmail.com>
2022-04-09 09:52:32 +0600

    dynamic card feature added in student list page, minor bug fixed

---
+++ js/load-data-in-list-page.js
@@ -0,0 +1,616 @@
+const student_data = [
+    {
+        // for bipul rabbi
+        img : "../img/gallery/bipul_hassan_rabbi.jpg",
+        name : "মোহাম্মদ রাব্বি",
+        roll : "451650",
+        profile : "../profiles/mohammad-rabbi-451650.html"
+    },
+
+    {
+        // for urmi
+        img : "../img/gallery/urmi.jpg",
+        name : "মৌমিতা ইসলাম ঊর্মি",
+        roll : "451706",
+        profile : "../profiles/moumita-islam-urmi-451706.html"
+    },
+
+    {
+        // for rameem
+        img : "../img/gallery/MAHMOOD HASSAN RAMEEM 1.jpg",
+        name : "মাহমুদ হাসান রামীম",
+        roll : "451638",
+        profile : "../profiles/mahmood-hassan-rameem-451638.html"
+    },
+
+    {
+        // for emon hossain
+        img : "../img/gallery/emon_hossain.jpg",
+        name : "মোহাম্মদ ইমন হোসেন",
+        roll : "451618",
+        profile : "../profiles/mohammad-emon-hossain-451618.html"
+    },
+
+    {
+        // for osama
+        img : "../img/gallery/undraw_male_avatar_323b.svg",
+        name : "ওয়াসিম আকরাম ওসামা",
+        roll : "451620",
+        profile : "#"
+    },
+
+    {
+        // for musanna
+        img : "../img/gallery/musanna.jpg",
+        name : "জাওহার মুসান্না",
+        roll : "451624",
+        profile : "../profiles/zawhar-musanna-451624.html"
+    },
+
+    {
+        // for gazi asel
+        img : "../img/gallery/gazi_rasel.jpg",
+        name : "গাজি রাসেল",
+        roll : "451625",
+        profile : "../profiles/gazi-rasel-451625.html"
+    },
+
+    {
+        // for baten
+        img : "../img/gallery/baten.jpg",
+        name : "আব্দুল বাতেন",
+        roll : "451627",
+        profile : "../profiles/abdul-baten-451627.html"
+    },
+
+    {
+        // for fahmida
+        img : "../img/gallery/fahmida.jpg",
+        name : "ফাহমিদা ইয়াসমিন",
+        roll : "451628",
+        profile : "../profiles/fahmida-yeasmin-451628.html"
+    },
+
+    {
+        // for mamun
+        img : "../img/gallery/mamun_sarkar.jpg",
+        name : "মামুন সরকার",
+        roll : "451629",
+        profile : "../profiles/mamun-sarker-451629.html"
+    },
+
+    {
+        // for senvia
+        img : "../img/gallery/senviya.jpg",
+        name : "সেংভিয়া চিরান",
+        roll : "451631",
+        profile : "../profiles/sangvia-chiran-451631.html"
+    },
+
+    {
+        // for tushar
+        img : "../img/gallery/tusar_ahmed.jpg",
+        name : "তুষার আহমেদ",
+        roll : "451632",
+        profile : "../profiles/tusar-ahmed-451632.html"
+    },
+
+    {
+        // for saykat
+        img : "../img/gallery/arefin_saykot.jpg",
+        name : "সুলতানুল আরেফিন",
+        roll : "451633",
+        profile : "../profiles/sultanul-arifen-451633.html"
+    },
+
+    {
+        // for noyon
+        img : "../img/gallery/noyon.jpg",
+        name : "নয়ন খলিফা",
+        roll : "451634",
+        profile : "../profiles/mohammad-noyon-khalifa-451634.html"
+    },
+
+    {
+        // for saad
+        img : "../img/gallery/saad.jpg",
+        name : "সাখাওয়াত হোসেন",
+        roll : "451635",
+        profile : "../profiles/mohammad-sakhawat-hossain-saad-451635.html"
+    },
+
+    {
+        // for rakib hk
+        img : "../img/gallery/rakib_hossain.jpg",
+        name : "মোঃ রাকিব হোসেন",
+        roll : "451636",
+        profile : "#"
+    },
+
+    {
+        // for abdullah
+        img : "../img/gallery/abu_salem_abdullah.jpg",
+        name : "মোহাম্মদ আবু সালেম আবদুল্লা",
+        roll : "451639",
+        profile : "#"
+    },
+
+    {
+        // for shishir
+        img : "../img/gallery/shishir.jpg",
+        name : "মোহাম্মদ শিশির আহমেদ",
+        roll : "451640",
+        profile : "../profiles/shishir-ahmed-451640.html"
+    },
+
+    {
+        // for rafi
+        img : "../img/gallery/jihad_mahmud.jpg",
+        name : "জিহাদ আল মাহমুদ",
+        roll : "451641",
+        profile : "../profiles/jihad-al-mahmud-451641.html"
+    },
+
+    {
+        // for mong
+        img : "../img/gallery/mong.jpg",
+        name : "মং মং সান",
+        roll : "451643",
+        profile : "../profiles/mong-mong-san-451643.html"
+    },
+
+    {
+        // for protik
+        img : "../img/gallery/protik_chandro.jpg",
+        name : "প্রতিক চন্দ্র বর্মন",
+        roll : "451644",
+        profile : "../profiles/protik-chandra-barman-451644.html"
+    },
+
+    {
+        // for usha
+        img : "../img/gallery/undraw_female_avatar_w3jk.svg",
+        name : "ঊষা সরকার",
+        roll : "451645",
+        profile : "../profiles/usha-sarkar-451645.html"
+    },
+
+    {
+        // for sayem
+        img : "../img/gallery/sayem.jpg",
+        name : "আবদুল্লাহ আল সায়েম",
+        roll : "451649",
+        profile : "#"
+    },
+
+    {
+        // for bithi
+        img : "../img/gallery/bithi.jpg",
+        name : "বিথি আক্তার",
+        roll : "451651",
+        profile : "../profiles/bithi-akter-451651.html"
+    },
+
+    {
+        // for mahabul
+        img : "../img/gallery/mahabul_alam.jpg",
+        name : "মোহাম্মদ মাহাবুল আলম",
+        roll : "451652",
+        profile : "../profiles/mohammad-mahabul-alam-451652.html"
+    },
+
+    {
+        // for nusrat
+        img : "../img/gallery/undraw_female_avatar_w3jk.svg",
+        name : "নুসরাত জাহান স্বর্ণা",
+        roll : "451655",
+        profile : "../profiles/nusrat-jahan-sorna-451655.html"
+    },
+
+    {
+        // for abdullah mamun
+        img : "../img/gallery/abdullah_mamun.jpg",
+        name : "আবদুল্লাহ আল মামুন",
+        roll : "451659",
+        profile : "#"
+    },
+
+    {
+        // for nafij
+        img : "../img/gallery/undraw_male_avatar_323b.svg",
+        name : "নাফিজ মাহমুদ",
+        roll : "451660",
+        profile : "#"
+    },
+
+    {
+        // for mohiuddin
+        img : "../img/gallery/mohiuddin.jpg",
+        name : "মোহাম্মদ মহিউদ্দিন",
+        roll : "451661",
+        profile : "#"
+    },
+
+    {
+        // for ria
+        img : "../img/gallery/rifatunnahar_mim.jpg",
+        name : "রিফাতুন নাহার মীম",
+        roll : "451663",
+        profile : "../profiles/refatun-naher-mim-451663.html"
+    },
+
+    {
+        // for sabina
+        img : "../img/gallery/sabina.jpg",
+        name : "সাবিনা আক্তার",
+        roll : "451665",
+        profile : "#"
+    },
+
+    {
+        // for riki
+        img : "../img/gallery/riki.jpg",
+        name : "রিকি ডেভিড গোমেজ",
+        roll : "451669",
+        profile : "#"
+    },
+
+    {
+        // for jahidul rasel
+        img : "../img/gallery/jahid_rasel.jpg",
+        name : "জাহিদুল ইসলাম রাসেল",
+        roll : "451670",
+        profile : "../profiles/jahedul-islam-rasel-451670.html"
+    },
+
+    {
+        // for najmul
+        img : "../img/gallery/nazmul-islam.jpg",
+        name : "মোহাম্মদ নাজমুল ইসলাম",
+        roll : "451671",
+        profile : "../profiles/najmul-islam-451671.html"
+    },
+
+    {
+        // for maysha
+        img : "../img/gallery/maysha.jpg",
+        name : "সায়মা",
+        roll : "451672",
+        profile : "../profiles/sayma-461672.html"
+    },
+
+    {
+        // for junayed
+        img : "../img/gallery/junaed.jpg",
+        name : "নাজমুস সাকিব জুনায়েদ",
+        roll : "451673",
+        profile : "#"
+    },
+
+    {
+        // for pias tushar
+        img : "../img/gallery/pias_tushar.jpg",
+        name : "পিয়াস মিয়া তুষার",
+        roll : "451675",
+        profile : "../profiles/pias-mia-tusher-451675.html"
+    },
+
+    {
+        // for jannat
+        img : "../img/gallery/undraw_female_avatar_w3jk.svg",
+        name : "জান্নাতুল ফেরদৌস",
+        roll : "451676",
+        profile : "#"
+    },
+
+    {
+        // for forid
+        img : "../img/gallery/forid.jpg",
+        name : "শেখ ফরিদ",
+        roll : "451677",
+        profile : "../profiles/shekh-farid-451677.html"
+    },
+
+    {
+        // for shihab
+        img : "../img/gallery/shihab.jpg",
+        name : "মোহাম্মদ মুহাইমিনুল ইসলাম শিহাব",
+        roll : "451680",
+        profile : "../profiles/mohammad-muhaiminul-islam-shihab-451680.html"
+    },
+
+    {
+        // for ritu
+        img : "../img/gallery/ritu_rahman.jpg",
+        name : "ঋতু রহমান",
+        roll : "451682",
+        profile : "../profiles/reetu-rahman-451682.html"
+    },
+
+    {
+        // for shithila
+        img : "../img/gallery/shithila.jpg",
+        name : "ফারজানা আক্তার শিথিলা",
+        roll : "451683",
+        profile : "../profiles/farzana-akter-shithila-451683.html"
+    },
+
+    {
+        // for masud
+        img : "../img/gallery/undraw_male_avatar_323b.svg",
+        name : "মোহাম্মদ মাসুদ আলি",
+        roll : "451684",
+        profile : "#"
+    },
+
+    {
+        // for khadija
+        img : "../img/gallery/undraw_female_avatar_w3jk.svg",
+        name : "খাদিজা আক্তার",
+        roll : "451685",
+        profile : "../profiles/khadiza-akter-451685.html"
+    },
+
+    {
+        // for sohel
+        img : "../img/gallery/sohel.jpg",
+        name : "মোহাম্মদ সোহেল রানা",
+        roll : "451686",
+        profile : "../profiles/mohammad-sohel-rana-451686.html"
+    },
+
+    {
+        // for sinthiya
+        img : "../img/gallery/sumiaya_sinthiya.jpg",
+        name : "সুমাইয়া আক্তার সিনথিয়া",
+        roll : "451687",
+        profile : "../profiles/sumaiya-akter-451687.html"
+    },
+
+    {
+        // for sakin
+        img : "../img/gallery/sakin.jpg",
+        name : "শাহরিয়ার আহমেদ সাকিন",
+        roll : "451688",
+        profile : "#"
+    },
+
+    {
+        // for sujon
+        img : "../img/gallery/sujon.jpg",
+        name : "মোহাম্মদ অহিদুল ইসলাম সুজন",
+        roll : "451689",
+        profile : "../profiles/mohammad-ahidul-islam-sujan-451689.html"
+    },
+
+    {
+        // for nayem kibriya
+        img : "../img/gallery/naim_kibriya.jpg",
+        name : "নাঈম কিবরিয়া",
+        roll : "451692",
+        profile : "../profiles/nayem-kibria-451692.html"
+    },
+
+    {
+        // for sanjid
+        img : "../img/gallery/sanjid.jpg",
+        name : "মোহাম্মদ সানজিদ আলি",
+        roll : "451694",
+        profile : "../profiles/mohammad-sanjid-451694.html"
+    },
+
+    {
+        // for rio
+        img : "../img/gallery/rio.jpg",
+        name : "মোহাম্মদ রাফাত হাসান",
+        roll : "451696",
+        profile : "#"
+    },
+
+    {
+        // for nazia
+        img : "../img/gallery/undraw_female_avatar_w3jk.svg",
+        name : "নাজিয়া সুলাতানা",
+        roll : "451698",
+        profile : "../profiles/nazia-sultana-451698.html"
+    },
+
+    {
+        // for mahadi
+        img : "../img/gallery/mahadi.jpg",
+        name : "মোহাম্মদ মাহাদি হাসান",
+        roll : "451699",
+        profile : "../profiles/mahadi-hasan-451699.html"
+    },
+
+    {
+        // for azan
+        img : "../img/gallery/undraw_male_avatar_323b.svg",
+        name : "রেদওয়ান রোকন নউশাদ",
+        roll : "451700",
+        profile : "../profiles/redowan-rukon-nowshad-451700.html"
+    },
+
+    {
+        // for sobahan
+        img : "../img/gallery/undraw_male_avatar_323b.svg",
+        name : "মোহাম্মদ সোবাহান মিয়া",
+        roll : "451701",
+        profile : "#"
+    },
+
+    {
+        // for tamim
+        img : "../img/gallery/sakib_tamim.jpg",
+        name : "সাকিবুর রাহমান তামিম",
+        roll : "451702",
+        profile : "../profiles/sakibur-rahman-tamim-451702.html"
+    },
+
+    {
+        // for azan
+        img : "../img/gallery/undraw_female_avatar_w3jk.svg",
+        name : "ফারিয়া ফেরদৌস জ্যোতি",
+        roll : "451704",
+        profile : "#"
+    },
+
+    {
+        // for rakib_raju
+        img : "../img/gallery/M_R_H_rakib.jpg",
+        name : "মোহাম্মদ রাকিব হোসেন",
+        roll : "451705",
+        profile : "../profiles/mohammad-rakib-hossain-451705.html"
+    },
+
+    {
+        // for sabbir
+        img : "../img/gallery/al_amin_sabbir.jpg",
+        name : "আল আমিন সাব্বির",
+        roll : "451707",
+        profile : "../profiles/al-amin-sabbir-451707.html"
+    },
+
+    {
+        // for fatema
+        img : "../img/gallery/fatema_koli.jpg",
+        name : "ফাতেমা বেগম",
+        roll : "451708",
+        profile : "../profiles/fatema-begum-451708.html"
+    },
+
+    {
+        // for hakim
+        img : "../img/gallery/hakim.jpg",
+        name : "মোহাম্মদ আব্দুল হাকিম",
+        roll : "451709",
+        profile : "../profiles/mohammad-abdul-hakim-451709.html"
+    },
+
+    {
+        // for aononto
+        img : "../img/gallery/anonto.jpg",
+        name : "মাহাতাব ইসলাম অনন্ত",
+        roll : "451714",
+        profile : "../profiles/mahatab-islam-aunonto-451714.html"
+    },
+
+    {
+        // for pollob
+        img : "../img/gallery/pollab.jpg",
+        name : "মোহাম্মদ রাকিব হোসেন পল্লব",
+        roll : "451722",
+        profile : "#"
+    },
+
+    {
+        // for likhon
+        img : "../img/gallery/likhon.jpg",
+        name : "লিখন আহমেদ",
+        roll : "451723",
+        profile : "../profiles/likhon-ahmed-492723.html"
+    },
+
+    {
+        // for sojol
+        img : "../img/gallery/shajal.jpg",
+        name : "মোহাম্মদ জাহিদুল ইসলাম সজল",
+        roll : "492725",
+        profile : "../profiles/mohammad-zahidul-islam-shajal-492725.html"
+    },
+
+    {
+        // for saifullah
+        img : "../img/gallery/saifullah.jpg",
+        name : "শেখ খালিদ সাইফুল্লাহ",
+        roll : "494191",
+        profile : "../profiles/sheikh-khalid-saifullah-494191.html"
+    },
+
+    {
+        // for rahul
+        img : "../img/gallery/rahul.jpg",
+        name : "তুষার রাহুল চিশিম",
+        roll : "494192",
+        profile : "../profiles/tusher-rahul-chisim-494192.html"
+    },
+
+    {
+        // for farhan
+        img : "../img/gallery/farhan.jpg",
+        name : "মোহাম্মদ হাবিবুর রহমান",
+        roll : "495021",
+        profile : "../profiles/habibur-rahman-495021.html"
+    },
+
+    {
+        // for chadni
+        img : "../img/gallery/chadni.jpg",
+        name : "সুমনা ইসলাম চাঁদনী",
+        roll : "495514",
+        profile : "../profiles/sumona-islam-chadni-495514.html"
+    },
+
+    {
+        // for mainuddin
+        img : "../img/gallery/mainuddin.jpg",
+        name : "মোহাম্মদ মাইনউদ্দিন",
+        roll : "184391",
+        profile : "../profiles/mohammad-mainuddin-184391.html"
+    },
+
+    {
+        // for pavel
+        img : "../img/gallery/pathan.jpg",
+        name : "মোহাম্মদ পাবেল পাঠান",
+        roll : "144812",
+        profile : "../profiles/mohammad-pabel-patan-144812.html"
+    },
+
+    {
+        // for josim
+        img : "../img/gallery/josim_uddin.jpg",
+        name : "মোহাম্মদ জসীম উদ্দিন",
+        roll : "185533",
+        profile : "#"
+    },
+
+    {
+        // for hasanur
+        img : "../img/gallery/hasanur.jpg",
+        name : "মোহাম্মদ হাসানুর রহমান",
+        roll : "981622",
+        profile : "../profiles/hasanur-rahman-981622.html"
+    },
+
+
+
+]
+
+
+const cardsElement = document.getElementById("list");
+
+const loadData = () => {
+    student_data.map(card => {
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("col-md-4");
+        cardElement.classList.add("col-sm-12");
+        cardElement.classList.add("d-block");
+        cardElement.classList.add("d-lg-flex");
+        cardElement.innerHTML = `
+            <div class="profile p-3">
+                <div class="pro-img">
+                    <img class="img-fluid" src="${card.img}" alt="">
+                </div>
+                <div class="pro-des">
+                    <h2 class="text-center">${card.name}</h2>
+                    <p class="text-center">রোলঃ ${card.roll}</p>
+                    <a class="btn btn-warning text-center d-block" href="${card.profile}">প্রোফাইল পড়ুন</a>
+                </div>
+            </div>
+        `;
+        cardsElement.appendChild(cardElement);
+    })
+}
+
+loadData()
