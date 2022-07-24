9964275dedfb1cc97c742c1855adbce713d2ec9e
rameem2003 <rameem2019@gmail.com>
2022-07-24 20:50:42 +0600

    Assignment updare

--- js/load-all-notice.js
+++ js/load-all-notice.js
@@ -27,7 +27,7 @@ const noticeBoard = [
         // 5th semester assignment
         title : "৫ম সেমিস্টার",
         subTitle : "Assignment এর আপডেট",
-        update : "সর্বশেষ আপডেট ২৩ জুলাই ২০২২",
+        update : "সর্বশেষ আপডেট ২৪ জুলাই ২০২২",
         link : "../others/assignment-update.html",
         btnColor : "btn-primary",
         downloadIcon : "",
