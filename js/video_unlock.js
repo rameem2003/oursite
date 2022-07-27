6bc04cce34c77537e7eb02509b82eaa06aec9e40
rameem2003 <rameem2019@gmail.com>
2022-07-27 15:18:59 +0600

    initial video upload

---
+++ js/video_unlock.js
@@ -0,0 +1,25 @@
+const unlock_btn = document.getElementById("unlock_btn");
+
+
+unlock_btn.addEventListener("click", () => {
+
+    const video = document.getElementById("pcb_video");
+
+    const input = document.getElementById("keys").value;
+
+    const error = document.getElementById("error_text");
+
+    password = "valobasha_obiram_legends";
+
+    if(input == password){
+        video.style.display = "block";
+        error.innerHTML = "";
+        input.style.display = "none";
+        unlock_btn.style.display = "none"
+    }else{
+        video.style.display = "none";
+        error.innerHTML = "Invalid password";
+    }
+})
+
+console.log(video);
