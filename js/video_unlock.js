d0c95b1dafc1c019688149e7d483822f0e113e34
rameem2003 <rameem2019@gmail.com>
2022-08-12 22:19:26 +0600

    vedio unlocked

--- js/video_unlock.js
+++ js/video_unlock.js
@@ -5,24 +5,29 @@ unlock_btn.addEventListener("click", () => {
 
     const video = document.getElementById("pcb_video");
 
-    const input = document.getElementById("keys").value;
+    // const input = document.getElementById("keys").value;
 
     const error = document.getElementById("error_text");
 
-    const password = "valobasha_obiram_legends";
-    const password_for_all = "istt_cmt_5th_pcb";
+    // const password = "valobasha_obiram_legends";
+    // const password_for_all = "istt_cmt_5th_pcb";
 
 
-    if(input == password || input == password_for_all){
-        video.style.display = "block";
-        video.src = '../videos/PCB/3D_circuit_design.mp4';
-        error.innerHTML = "";
-        input.style.display = "none";
-        unlock_btn.style.display = "none"
-    }else{
-        video.style.display = "none";
-        error.innerHTML = "Invalid password";
-    }
+    // if(input == password || input == password_for_all){
+    //     video.style.display = "block";
+    //     video.src = '../videos/PCB/3D_circuit_design.mp4';
+    //     error.innerHTML = "";
+    //     input.style.display = "none";
+    //     unlock_btn.style.display = "none"
+    // }else{
+    //     video.style.display = "none";
+    //     error.innerHTML = "Invalid password";
+    // }
+
+
+    video.style.display = "block";
+    video.src = '../videos/PCB/3D_circuit_design.mp4';
+    error.innerHTML = "";
 })
 
-console.log(video);
+// console.log(video);
