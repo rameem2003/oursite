d1f617184f0d6bafc39eb03d17a985b8a530f03d
rameem2003 <rameem2019@gmail.com>
2022-09-08 14:50:57 +0600

    clock added and greeting message added

---
+++ js/home-clock.js
@@ -0,0 +1,58 @@
+function home_clock(){
+    let date = new Date();
+
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+
+    var formate = document.getElementById("formate");
+
+    var greeting = document.getElementById("greeting");
+
+
+    // greeting setup
+    if(hour >= 6 && hour < 12){
+        greeting.innerHTML = "শুভ সকাল"
+    }
+
+    if(hour >= 12 && hour <= 17){
+        greeting.innerHTML = "শুভ অপরাহ্ণ"
+    }
+
+    if(hour >= 18){
+        greeting,innerHTML = "শুভ রাত্রি"
+    }
+
+    // AMPM Setup
+
+    if(hour == 0){
+        hour = 12;
+    }
+    if(hour < 12){
+        formate.innerHTML = "AM";
+    }
+    if(hour > 12){
+        hour = hour - 12;
+        formate.innerHTML = "PM";
+    }
+
+    // formate minute
+    if(hour < 10){
+        hour = '0' + hour;
+    }
+
+
+    if(minute < 10){
+        minute = '0' + minute;
+    }
+
+    if(second < 10){
+        second = '0' + second;
+    }
+
+    document.getElementById("clock").innerHTML = `এখন সময় ${hour} : ${minute} : ${second} &nbsp`;
+
+    setInterval(home_clock, 1000);
+}
+
+home_clock();
