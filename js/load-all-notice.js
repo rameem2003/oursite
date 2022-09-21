f9e7d460a4f7441217d7436bdc70d0a4a142df78
rameem2003 <rameem2019@gmail.com>
2022-09-21 22:07:43 +0600

    date update others page

--- js/load-all-notice.js
+++ js/load-all-notice.js
@@ -28,7 +28,7 @@ const noticeBoard = [
         // 5th semester assignment
         title : "৫ম সেমিস্টার",
         subTitle : "Assignment এর আপডেট",
-        update : "সর্বশেষ আপডেট ১৪ আগস্ট ২০২২",
+        update : "সর্বশেষ আপডেট ২১ আগস্ট ২০২২",
         link : "../others/assignment-update.html",
         btnColor : "btn-primary",
         downloadIcon : "",
