9d1be1bb8a55db13df1b3ed69dae6e9674087a5a
rameem2003 <rameem2019@gmail.com>
2022-10-13 20:35:49 +0600

    update

--- js/load-all-notice.js
+++ js/load-all-notice.js
@@ -3,7 +3,7 @@ const noticeBoard = [
         // 6th semester class routine
         title : "৬ষ্ঠ সেমিস্টার",
         subTitle : "ক্লাস রুটিন",
-        update : "সর্বশেষ আপডেট ১০ অক্টোবর ২০২২",
+        update : "সর্বশেষ আপডেট ১৩ অক্টোবর ২০২২",
         link : "../docs/6th Semester class routine.pdf",
         btnColor : "btn-danger",
         downloadIcon : `<i class="fas fa-download"></i>`,
