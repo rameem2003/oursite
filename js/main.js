f3f3160d6c5ac4fa7927b4ab1d13d583bef4b26b
rameem2003 <rameem2019@gmail.com>
2023-09-13 21:02:27 +0600

    probably last update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-document.getElementById("rights").innerHTML = "&copy; ২০২২-২০২৩ মাহমুদ হাসান রামীম। সর্বসত্ত্ব সংরক্ষিত।";
+const year = (y) => {
+  const number = "০১২৩৪৫৬৭৮৯";
+  let input = String(y);
+
+  let result = "";
+
+  for (let i = 0; i < input.length; i++) {
+    result = result + number[Number(input[i])];
+  }
+
+  return result;
+};
+
+document.getElementById("rights").innerHTML = `&copy; ২০২২-${year(
+  new Date().getFullYear()
+)} মাহমুদ হাসান রামীম। সর্বসত্ত্ব সংরক্ষিত।`;
 
 $("#hide_text").hide(); // from index.html
 $("#php_code").hide(); // form web development assignment-update.html
@@ -6,13 +21,7 @@ $("#C_S_Code_method_overloading").hide(); // form oop assignment-update.html
 $("#C_S_Code_inheritance").hide(); // form oop assignment-update.html
 $("#bootstrap_code").hide(); // form web development assignment-update.html
 
-
-// index page hide text 
-$("#hide_btn").click(function(){
-    $("#hide_text").slideToggle(); // from index.html
+// index page hide text
+$("#hide_btn").click(function () {
+  $("#hide_text").slideToggle(); // from index.html
 });
-
-
-
-
-
